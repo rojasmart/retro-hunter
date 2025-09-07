e6a268dc454a87c76fb00a9f348a025d846e8024
rojasmart <rogeriosvaldo@gmail.com>
2025-09-07 09:31:40 +0100

    --add new dataset for platform and new alias

--- backend/main.py
+++ backend/main.py
@@ -28,8 +28,35 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Dicionário de plataforma para dataset
 platform_to_dataset = {
+    "Amiga": os.path.join(BASE_DIR, "datasets", "amiga.json"),
+    "Dreamcast": os.path.join(BASE_DIR, "datasets", "dreamcast.json"),
+    "Gameboy Advance": os.path.join(BASE_DIR, "datasets", "gameboy_advance.json"),
+    "Gameboy Color": os.path.join(BASE_DIR, "datasets", "gameboy_color.json"),
+    "Gamegear": os.path.join(BASE_DIR, "datasets", "game_gear.json"),
+    "Neo Geo": os.path.join(BASE_DIR, "datasets", "neo_geo.json"),
+    "Nintendo": os.path.join(BASE_DIR, "datasets", "nintendo.json"),
+    "Nintendo Switch": os.path.join(BASE_DIR, "datasets", "nintendo_switch.json"),
+    "Nintendo 64": os.path.join(BASE_DIR, "datasets", "nintendo_64.json"),
+    "Nintendo Wii": os.path.join(BASE_DIR, "datasets", "nintendo_wii.json"),
+    "Nintendo Wii U": os.path.join(BASE_DIR, "datasets", "nintendo_wii_u.json"),
+    "Nintendo 3DS": os.path.join(BASE_DIR, "datasets", "nintendo_3ds.json"),  
+    "Nintendo DS": os.path.join(BASE_DIR, "datasets", "nintendo_ds.json"),
     "Playstation 1": os.path.join(BASE_DIR, "datasets", "playstation_1.json"),
     "Playstation 2": os.path.join(BASE_DIR, "datasets", "playstation_2.json"),
+    "Playstation 3": os.path.join(BASE_DIR, "datasets", "playstation_3.json"),
+    "Playstation 4": os.path.join(BASE_DIR, "datasets", "playstation_4.json"),
+    "Playstation 5": os.path.join(BASE_DIR, "datasets", "playstation_5.json"),
+    "Playstation Vita": os.path.join(BASE_DIR, "datasets", "playstation_vita.json"),
+    "Psp": os.path.join(BASE_DIR, "datasets", "psp.json"),
+    "Sega Master System": os.path.join(BASE_DIR, "datasets", "sega_master_system.json"),
+    "Sega Megadrive": os.path.join(BASE_DIR, "datasets", "sega_megadrive.json"),
+    "Sega Saturn": os.path.join(BASE_DIR, "datasets", "sega_saturn.json"),
+    "Xbox": os.path.join(BASE_DIR, "datasets", "xbox.json"),
+    "Xbox 360": os.path.join(BASE_DIR, "datasets", "xbox_360.json"),
+    "Xbox One": os.path.join(BASE_DIR, "datasets", "xbox_one.json"),
+    "Xbox Series": os.path.join(BASE_DIR, "datasets", "xbox_series.json"),
+    "Zx Spectrum": os.path.join(BASE_DIR, "datasets", "zx_spectrum.json"),
+  
     # Adicione outras plataformas conforme necessário
 }
 
